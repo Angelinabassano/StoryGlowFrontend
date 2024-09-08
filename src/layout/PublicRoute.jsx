8f20120402410c88c3e9296d24b7d03c64c51573
AngelinaBassano <bassanoangelina2002@gmail.com>
2024-09-08 13:52:22 +0200

    SG-117 Create Footer

--- src/layout/PublicRoute.jsx
+++ src/layout/PublicRoute.jsx
@@ -1,13 +1,16 @@
+import Footer from '@/components/footer/Footer';
 import NavbarPublic from '@/components/navbar/NavbarPublic';
 import { Outlet } from 'react-router-dom';
 
 const PublicRoute = () => {
   return (
-    <>
-      <NavbarPublic/>
-      <Outlet />
-      {/*footer*/}
-    </>
+    <div className="flex flex-col min-h-screen">
+      <NavbarPublic />
+      <main className="flex-grow flex items-center justify-center px-4">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
   );
 };
 
