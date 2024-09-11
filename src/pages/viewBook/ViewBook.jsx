dc221d2f448eafe1f1bd5a350183a16fb5375b2f
AngelinaBassano <bassanoangelina2002@gmail.com>
2024-09-11 13:16:11 +0200

    SG-59 and 58 delete book

--- src/pages/viewBook/ViewBook.jsx
+++ src/pages/viewBook/ViewBook.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 const ViewBook = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
   const [book, setBook] = useState(null);
 
   useEffect(() => {
@@ -19,6 +20,16 @@ const ViewBook = () => {
     fetchBook();
   }, [id]);
 
+  const deleteBook = async () => {
+    try {
+      await axios.delete(`http://127.0.0.1:8000/api/v1/books/delete/${id}/`);
+      navigate('/homeprotected'); 
+    } catch (error) {
+      console.error('Error deleting book:', error);
+      alert('Error deleting book');
+    }
+  };
+
   if (!book) {
     return null;
   }
@@ -26,9 +37,29 @@ const ViewBook = () => {
   const imageUrl = book.cover_photo || 'path/to/default_image.jpg';
 
   return (
-    <div className="max-w-4xl mx-auto bg-white text-black shadow-lg rounded-lg overflow-hidden border border-gray-300">
-      <div className="flex flex-col lg:flex-row items-center lg:items-start pt-6 lg:pt-8">
-        <div className="w-full lg:w-1/3 flex justify-center lg:pl-4 pt-4 lg:pt-0">
+    <div className="relative mx-auto bg-white text-black shadow-lg rounded-lg overflow-hidden border border-gray-300 max-w-full lg:max-w-4xl">
+      <button
+        onClick={deleteBook}
+        className="absolute top-4 right-4 bg-primary text-white p-1 rounded-full"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </svg>
+      </button>
+
+      <div className="flex flex-col lg:flex-row items-center lg:items-start p-4 lg:p-8">
+        <div className="w-full lg:w-1/2 flex justify-center lg:pr-4 mb-4 lg:mb-0">
           <img
             className="w-48 h-72 object-cover rounded-lg shadow-md"
             src={imageUrl}
@@ -39,7 +70,7 @@ const ViewBook = () => {
             }}
           />
         </div>
-        <div className="w-full lg:w-2/3 p-4 flex flex-col justify-between pt-4 lg:pt-0">
+        <div className="w-full lg:w-1/2 p-4 flex flex-col justify-between">
           <h2 className="text-2xl font-semibold mb-2 text-center lg:text-left text-primary">
             {book.title || 'Book Title'}
           </h2>
@@ -50,7 +81,7 @@ const ViewBook = () => {
           </div>
           <button
             onClick={() => alert(`Reading ${book.title}`)}
-            className="bg-white text-primary px-4 py-2 rounded-lg border border-primary hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-primary mt-4"
+            className="bg-primary text-white px-4 py-2 rounded-lg border mt-4"
           >
             Read
           </button>
